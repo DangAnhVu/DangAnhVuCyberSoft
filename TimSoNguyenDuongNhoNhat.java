89d4fdcf488721075e1f181ef39ddeb2c076b0cd
DangAnhVu <93905398+DangAnhVu@users.noreply.github.com>
2024-08-13 10:07:09 +0700

    TimSoNguyenDuongNhoNhat

---
+++ TimSoNguyenDuongNhoNhat.java
@@ -0,0 +1,14 @@
+
+public class TimSoNguyenDuongNhoNhat {
+
+	public static void main(String[] args) {
+		int n=0;
+		int sum=0;
+		while(sum<10000) {
+			n+=1;
+			sum+=n;			
+		}
+		System.out.println("Số nhỏ nhất thỏa điều kiện là: "+n);
+	}
+
+}
