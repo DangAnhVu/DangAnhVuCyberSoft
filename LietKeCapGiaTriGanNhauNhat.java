178b54f7a2f5957a17465584b1c3a7c2dfe28276
DangAnhVu <93905398+DangAnhVu@users.noreply.github.com>
2024-08-22 09:51:47 +0700

    LietKeCapGiaTriGanNhauNhat

---
+++ LietKeCapGiaTriGanNhauNhat.java
@@ -0,0 +1,67 @@
+import java.util.Scanner;
+
+public class LietKeCapGiaTriGanNhauNhat {
+
+	final static Scanner sc = new Scanner(System.in);
+	
+	public static void main(String[] args) {
+		int n=NhapN();
+		int[] a = new int[n];
+		NhapMang(a, n);
+		XuatMangint(a, n);
+		Sort(a, n);
+		LietKe(a, n);
+	}
+	
+	public static int NhapN() {
+		int n;
+		do {
+			System.out.println("Nhap n : ");
+			n = sc.nextInt();
+		} while (n<2);
+		return n;
+	}
+	
+	public static void NhapMang(int[] a, int n) {
+		for(int i=0; i<n; i++) {
+			System.out.print("a["+i+"] = ");
+			a[i]=sc.nextInt();
+		}
+	}
+	
+	public static void XuatMangint(int[] a, int n) {
+		System.out.println("Danh sách các phần tử trong mảng: ");
+		for(int i=0; i<n; i++) {
+			System.out.print(a[i]+"\t");
+		}
+		System.out.println("");
+	}
+	
+	public static void Sort(int[] a, int n) {
+		for(int i=0;i<n-1;i++) {
+			for(int j=i+1;j<n;j++) {
+				if(a[i]>a[j]) {
+					int temp = a[i];
+					a[i]=a[j];
+					a[j]=temp;
+				}
+			}
+		}
+	}
+	
+	public static void LietKe(int[] a, int n) {
+		int min_minus = a[1]-a[0] ;
+		for(int i=1;i<n-1;i++) {
+			int minus = a[i+1] - a[i];
+			if(min_minus>minus) {
+				min_minus = minus;
+			}
+		}
+		for(int i=0;i<n-1;i++) {
+			if(a[i+1] - a[i] == min_minus) {
+				System.out.print("("+a[i]+","+a[i+1]+")\t");
+			}
+		}
+	}
+
+}
