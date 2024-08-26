95faf82b69992631e874fa55bffaec8785360dd9
DangAnhVu <93905398+DangAnhVu@users.noreply.github.com>
2024-08-26 10:49:08 +0700

    TinhTongCacPhanTuChanTrongMaTran

---
+++ TinhTongCacPhanTuChanTrongMaTran.java
@@ -0,0 +1,60 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class TinhTongCacPhanTuChanTrongMaTran {
+
+	final static Scanner sc = new Scanner(System.in);
+	final static Random rd = new Random();
+
+	public static void main(String[] args) {
+		int[][] a = new int[100][100];
+		int m = NhapM();
+		int n = NhapN();
+		NhapMaTran(a, m, n);
+		XuatMaTran(a, m, n);
+		TinhTongSoChan(a, m, n);
+	}
+	
+	public static int NhapM() {
+		System.out.println("Nhập số dòng:");
+		return sc.nextInt();
+	}
+	
+	
+	public static int NhapN() {
+		System.out.println("Nhập số cột:");
+		return sc.nextInt();
+	}
+	
+	public static void NhapMaTran(int[][] a, int m, int n) {
+		System.out.println("Nhập ma trận:");
+		for(int i=0;i<m;i++) {
+			for(int j=0;j<n;j++) {	
+				a[i][j]= rd.nextInt((50 - (-50) + 1)) + (-50);
+			}
+		}
+	}
+	
+	public static void XuatMaTran(int[][] a, int m, int n) {
+		System.out.println("Xuất ma trận:");
+		for(int i=0;i<m;i++) {
+			for(int j=0;j<n;j++) {
+				System.out.print(a[i][j]+"\t");
+			}
+			System.out.println();
+		}
+	}
+	
+	public static void TinhTongSoChan(int[][] a, int m, int n) {
+		int sum = 0;
+		for(int i=0;i<m;i++) {
+			for(int j=0;j<n;j++) {
+				if(a[i][j] %2 == 0) {
+					sum+=a[i][j];
+				}
+			}
+		}
+		System.out.println("Tổng của các giá trị số chẵn trong mảng 2 chiều là: "+ sum);
+	}
+
+}
